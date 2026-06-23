8b9410f68191d1da33c243168741101ebfdf91a5
Zenia <zeniamukhuti16@gmail.com>
2026-06-23 20:24:28 +0530

    practice javascript

---
+++ basics.js
@@ -0,0 +1,30 @@
+// node: run time environment for js
+// to run js program: <node filename>
+// Variables
+// data types
+let x, y, z // const variables can not be reinitialized but let can be reinitialized.
+// let x
+// let y
+// let z
+let o0ly
+x = 7 // int 
+y = 5 // int
+
+z = x + y // int
+d = "Zenia" + "Mukhuti"
+console.log("z value:", z, d)
+console.log("Good night")
+// OPerators : = , +, -, /
+
+
+x = 10;
+y =50;
+
+let sum = x * y;
+
+console.log(sum);
+
+const a = 20;
+const b = 60;
+const c = a + b;
+console.log("c", c);
