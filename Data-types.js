d0e62475fdc7cff259f911c357c347790c25446b
Zenia <zeniamukhuti16@gmail.com>
2026-06-24 14:32:58 +0530

    variable.js

---
+++ Data-types.js
@@ -0,0 +1,37 @@
+// data types in javascript means what type of data is stored.
+// think od variable is box and data types tell us whatts inside the box.
+// There are eight basic data types in JavaScript.
+
+// 1. Number:
+// numbers can be a whole number or it can be decimals.
+let age = "24";
+let price = "89.99";
+// so here 24, and 89.99 are numbers.
+
+// 2. BigInt:
+// normaly javascript use Numbers type for number.
+// ex:
+let num = "123";
+// so javscript can handle numbers up to some extenend.
+// but when we need larger number then that , we use BigInt.
+//  to create BigInt we add "n" at the end of the number.
+// ex:
+let bignumber = "123456789012345678901234567890n";
+
+// 3. String:
+// Basically string is a text that written inside quote.
+// ex:
+let massage = "hello!";
+let userName = "i'm Zenia";
+// so here "hello" and "zenia" are strings.
+
+// 4. Boolen:
+// Boolen have only two values:
+// true and false.
+// ex:
+let nameFiledChecked = true;
+let ageFiledchecked = false;
+// think of it as yes or no, on and off.
+
+// 5. Null:
+// 
---
+++ variable.js
@@ -0,0 +1,37 @@
+// variable is a container used to store data.
+// value can change anytime.
+// to create variable we use let keyword in javascript.
+// let.
+
+let message;
+// let is a variable and massage is the variable name.
+// so simply creates a variable named massage but dosen't have any value yet.
+
+let massage;
+massage = "hello!";
+// so this means it created a box or container named massage and the value of the box is hello.
+
+// we can also create variable declaration in single line.
+let massage = "hello!";
+
+// we can also declare multiple variables in one single line.
+let massage = "hello!", user = "Zenia", age = "24";
+// but its not recommended for the sake of better readablity.
+// so its better to use it for easy readablity.
+let massage = "hello!";
+let user = "Zenia";
+let age = "24";
+
+// Constants.
+// constant is a value that never change after it's created.
+const mybirthday = "16.07.2002";
+// why we used const, when we know that the value inside the box will never change.
+// in let variable we can change the value latter also,
+// but in const we cant change the value, if we change it it will cause an error.
+// for expamle:
+const userName = "Zenia"; 
+// name wont change.
+let age = "24";
+// can change it latter.
+
+// so a const is simple variable whose value never change or cannot change after its created.
