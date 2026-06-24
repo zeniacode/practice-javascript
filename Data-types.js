c3c394382a6719bd17037eb6d2cec5ec16cd0201
Zenia <zeniamukhuti16@gmail.com>
2026-06-24 23:35:29 +0530

    change variable names

--- Data-types.js
+++ Data-types.js
@@ -4,19 +4,19 @@
 
 // 1. Number:
 // numbers can be a whole number or it can be decimals.
-let age = "24";
-let price = "89.99";
+let age1 = 24;
+let price = 89.99;
 // so here 24, and 89.99 are numbers.
 
 // 2. BigInt:
 // normaly javascript use Numbers type for number.
 // ex:
-let num = "123";
+let num = 123;
 // so javscript can handle numbers up to some extenend.
 // but when we need larger number then that , we use BigInt.
 //  to create BigInt we add "n" at the end of the number.
 // ex:
-let bignumber = "123456789012345678901234567890n";
+let bignumber = 123456789012345678901234567890n;
 
 // 3. String:
 // Basically string is a text that written inside quote.
@@ -24,6 +24,18 @@ let bignumber = "123456789012345678901234567890n";
 let massage = "hello!";
 let userName = "i'm Zenia";
 // so here "hello" and "zenia" are strings.
+
+let name3 = "John";
+
+console.log("My name is name3")
+
+// embed a variable
+console.log( `Hello, ${name3}` ); // Hello, John!
+
+// embed an expression
+console.log( `the result is ${1 + 2}` ); // the result is 3
+
+
 
 // 4. Boolen:
 // Boolen have only two values:
@@ -35,13 +47,13 @@ let ageFiledchecked = false;
 
 // 5. Null:
 // null means "nothing".
-let name = "null";
+let name1 = "null";
 // this telling javascript that this variable has no value currently.
 // It’s just a special value which represents “nothing”, “empty” or “value unknown”.
 
 // 6. undefined:
 // Udefined means "A value has not been given yet".
-let x;
+let x1;
 // if a variable declared and not assinged then the value is undefined.
 // if we do this 
 let x;
@@ -49,7 +61,7 @@ console.log(x);
 // the output will be undefined.
 
 // undefined vs Null:
-let phoneNumber;
+let phoneNumber1;
 // it means that varilable is created but the value is not given.
 let phoneNumber = "null";
 // it means that variable is created but value is not given Intentionally.
@@ -60,16 +72,23 @@ let phoneNumber = "null";
 let name = "Zenia";
 let age = 24;
 let city = "siliguri";
+console.log(age)
 // we have 3 separete variables.
 
 // ex: with object,
-let information ={
-     name = "Zenia",
-     age = "24",
-     city = "siliguri",
+let information = {
+     name : "Zenia",
+     age : 24,
+     city : "siliguri",
 }
+console.log(information)
 // now all the information is stored in on object.
 // in simple word we can think object as a folder were we can put things togathere.
 
 // The typeof operator:
+// typeof operator is used to find out the data type of variable or data.
+
+console.log(typeof 3)
+console.log(typeof information)
+console.log(typeof "information")
 
--- variable.js
+++ variable.js
@@ -7,31 +7,36 @@ let message;
 // let is a variable and massage is the variable name.
 // so simply creates a variable named massage but dosen't have any value yet.
 
-let massage;
-massage = "hello!";
+let massage1;
+massage1 = "hello!";
 // so this means it created a box or container named massage and the value of the box is hello.
 
 // we can also create variable declaration in single line.
-let massage = "hello!";
+let massage2 = "hello!";
 
 // we can also declare multiple variables in one single line.
-let massage = "hello!", user = "Zenia", age = "24";
+let massage3 = "hello!", user = "Zenia", age = "24";
 // but its not recommended for the sake of better readablity.
 // so its better to use it for easy readablity.
-let massage = "hello!";
-let user = "Zenia";
-let age = "24";
+let massage4 = "hello!";
+let user1 = "Zenia";
+let age1 = "24";
 
 // Constants.
 // constant is a value that never change after it's created.
-const mybirthday = "16.07.2002";
+const myBirthday = "16.07.2002";
 // why we used const, when we know that the value inside the box will never change.
 // in let variable we can change the value latter also,
 // but in const we cant change the value, if we change it it will cause an error.
 // for expamle:
 const userName = "Zenia"; 
 // name wont change.
-let age = "24";
+let age2 = "24";
 // can change it latter.
 
 // so a const is simple variable whose value never change or cannot change after its created.
+
+let name, admin
+name = "john";
+admin = name; // this means to tell comp to copy name value to admin var.
+console.log(admin,name);
