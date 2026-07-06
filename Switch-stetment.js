c50770daeff6ed91acb28832aedcf4725d0a081e
Zenia <zeniamukhuti16@gmail.com>
2026-07-06 18:46:59 +0530

    .\Loops.js

---
+++ Switch-stetment.js
@@ -0,0 +1,21 @@
+// the switch stetment :
+// this stetment "switch" is way to exucute multiple block of code on basis od valu and variable.
+// the syntax od it :
+// switch (expression) {
+//   case value1:
+//      Code to run if expression === value1
+//     break;
+
+//   case value2:
+//     Code to run if expression === value2
+//     break;
+
+//   case value3:
+//      Code to run if expression === value3
+//     break;
+
+//   default:
+//      Code to run if none of the cases match
+// }
+
+// ex:
